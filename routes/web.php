534565fbf5445f423bb4be06a0d40fb56f71aab5
sajjad6708 <s313dasht@gmail.com>
2024-05-14 22:19:22 +0330

    add view

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminDashboardController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->namespace('Admin')->group(function(){
+Route::get('/' ,[AdminDashboardController::class , 'index'])->name('admin.home') ;
+
+Route::prefix('market')->namespace('Market')->group(function(){
+
+//Category
+
+// Route::resource( 'category' ,[CategoryController::class]);
+
+});
+
 });
